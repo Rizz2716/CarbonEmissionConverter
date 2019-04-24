4d0914a01ae0916a70cfc893646ff79ae2c59bf6
Rizz2716 <41773993+Rizz2716@users.noreply.github.com>
2019-04-24 15:43:24 +0800

    new app

--- app/src/main/java/com/example/carbonemissionconverter/SettingActivity.java
+++ app/src/main/java/com/example/carbonemissionconverter/SettingActivity.java
@@ -1,10 +1,13 @@
 package com.example.carbonemissionconverter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class SettingActivity extends AppCompatActivity {
@@ -15,6 +18,25 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
